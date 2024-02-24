9353953fb8871c7d9b1a5a42c29f7abbeb9b2976
Zhu Pingzeyu <2510036348@qq.com>
2024-02-24 20:21:24 +0800

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import draw_form as a
+
+mes_n = ["Name","Phone Number","Email Address"]
+
+mes = [mes_n,
+["FoFoNeedle","13886621483","2510036348@qq.com"],
+["Lisa","14456782343","12677888888888876768768766@gg.com"]]
+
+def main():
+    a.d_f(mes)
+
+if __name__ == "__main__":
+    main()
